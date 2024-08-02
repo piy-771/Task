581dc09f60d3db91b2a7d0551ba54da3c340dc82
Piyush Sahu <piyushsahu2103@gmail.com>
2024-08-02 22:31:16 +0530

    Task

---
+++ src/FirstTenPrime.java
@@ -0,0 +1,21 @@
+public class FirstTenPrime {
+    public static void main(String[] args) {
+        int count1 = 0;
+
+        int i = 2;
+        while (count1 < 10){
+            int count = 0;
+            for(int j = 2;j<i;j++){
+                if(i % j == 0){
+                    count++;
+
+                }
+            }
+            if(count == 0){
+                System.out.println(i);
+                count1++;
+            }
+            i++;
+        }
+    }
+}
